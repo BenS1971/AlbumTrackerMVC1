278aafaa2fdb3ac6835eec233681ed1b329eb2c1
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-08 19:29:34 -0400

    More stuff added. Okay?

--- AlbumTrackerMVC1.Data/ApplicationDbContext.cs
+++ AlbumTrackerMVC1.Data/ApplicationDbContext.cs
@@ -1,8 +1,9 @@
+using AlbumTrackerMVC1.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 
 namespace AlbumTrackerMVC1.Data;
 
-public class ApplicationDbContext
-{
-    
-}
