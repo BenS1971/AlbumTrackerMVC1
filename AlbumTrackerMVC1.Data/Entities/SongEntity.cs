84c10f943b671dc83fbc9f9c6fc6213fdde5409f
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-14 20:39:30 -0400

    Here come them entities!

--- AlbumTrackerMVC1.Data/Entities/SongEntity.cs
+++ AlbumTrackerMVC1.Data/Entities/SongEntity.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 
@@ -6,6 +7,18 @@ namespace AlbumTrackerMVC1.Data.Entities;
 
 public class SongEntity
 {
-   
+   [Required]
+   public int AlbumId { get; set;}
+
+   [MaxLength(200)]
+   public string? AlbumName { get; set; }
+
+  
+   public string? ArtistName { get; set;}
+
+   [MaxLength(200)]
+   public required string SongName { get; set; }
+
+   public decimal? SongDuration { get; set;}
 
 }
