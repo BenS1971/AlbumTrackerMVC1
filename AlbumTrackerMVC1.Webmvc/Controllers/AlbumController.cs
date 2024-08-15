aae7b4da1d5a2a50d255938cf6954e05f4829120
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-15 18:48:23 -0400

    More controller stuff...

--- AlbumTrackerMVC1.Webmvc/Controllers/AlbumController.cs
+++ AlbumTrackerMVC1.Webmvc/Controllers/AlbumController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace AlbumTrackerMVC1.Webmvc.Controllers;
 
 public class AlbumController
 {
--- AlbumTrackerMVC1.Webmvc/Controllers/SongController.cs
+++ AlbumTrackerMVC1.Webmvc/Controllers/SongController.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlbumTrackerMVC1.Webmvc.Controllers;
 
 
 public class SongController
