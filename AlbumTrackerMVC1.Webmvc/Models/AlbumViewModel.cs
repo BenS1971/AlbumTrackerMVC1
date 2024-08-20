923321c0ad4b6cc3b82110f3ed72e6fbb21da1d0
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-20 18:24:24 -0400

    Here come the models. (not SUPER models!)

---
+++ AlbumTrackerMVC1.Webmvc/Models/AlbumViewModel.cs
@@ -0,0 +1,14 @@
+namespace AlbumTrackerMVC1.Webmvc.Models;
+
+public class AlbumViewModel
+{
+    public string? AlbumName { get; set; }
+    public string? AlbumArtist { get; set; }
+    public string? ArtistName { get; set; }
+    public string? Format {get; set; }
+    public string? Genre { get; set; }
+
+    public DateTime ReleaseYear {get; set;}
+
+
+}
---
+++ AlbumTrackerMVC1.Webmvc/Models/ArtistViewModel.cs
@@ -0,0 +1,8 @@
+namespace AlbumTrackerMVC1.Webmvc.Models;
+
+public class ArtistViewModel
+{
+    required public string ArtistName { get; set;}
+    public string? Format {get; set; }
+    public string? Genre { get; set;}
+}
