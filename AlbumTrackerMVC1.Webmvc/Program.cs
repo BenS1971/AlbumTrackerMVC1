665ab0c39ce3a951588f18816a45998acf6df346
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-21 20:28:48 -0400

    Fixing a boo-boo.

--- AlbumTrackerMVC1.Webmvc/Program.cs
+++ AlbumTrackerMVC1.Webmvc/Program.cs
@@ -8,14 +8,13 @@ using AlbumTrackerMVC1.Services.Songs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddMvc();
-
-builder.Services.AddScoped<IAlbumService, AlbumsService>();
+// builder.Services.AddScoped<IAlbumService, AlbumService>();
 
 builder.Services.AddScoped<IArtistsService, ArtistsService>();
 
@@ -23,11 +22,11 @@ builder.Services.AddScoped<ISongsService, SongsService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
