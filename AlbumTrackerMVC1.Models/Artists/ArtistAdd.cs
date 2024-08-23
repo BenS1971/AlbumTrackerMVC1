33a47f0836019fb0773876a353ea4f969c913788
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-22 21:12:40 -0400

    Filling in more models. (Whistle.)

--- AlbumTrackerMVC1.Models/Artists/ArtistAdd.cs
+++ AlbumTrackerMVC1.Models/Artists/ArtistAdd.cs
@@ -6,5 +6,11 @@ namespace AlbumTrackerMVC1.Models.Artists;
 
 public class ArtistAdd 
 {
-    
+    [Required]
+    [StringLength(200)]
+    public string ArtistName {get; set;} = string.Empty;
+
+    public string? Format { get; set; } = string.Empty;
+
+    public string? Genre { get; set; } = string.Empty;
 }
--- AlbumTrackerMVC1.Models/Songs/AddSong.cs
+++ AlbumTrackerMVC1.Models/Songs/AddSong.cs
@@ -7,5 +7,16 @@ namespace AlbumTrackerMVC1.Models.Songs;
 // Here is the model for adding a song to the song page.
 public class AddSong 
 {
-    
+    [Required]
+    [StringLength(200)]
+    public string SongName {get; set; } = string.Empty;
+
+    [StringLength(200)]
+    public string? AlbumName { get; set; } = string.Empty;
+
+    public string? ArtistName { get; set; } = string.Empty;
+
+    public double? SongDuration { get; set; } = double.MaxValue;
+
+
 }
