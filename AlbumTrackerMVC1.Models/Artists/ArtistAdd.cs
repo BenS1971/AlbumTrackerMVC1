6bca6f86a9ae5337e47a91604c0e27ec3e217292
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-22 20:50:33 -0400

    Stuff finally getting done! (Wonder of wonders!)

---
+++ AlbumTrackerMVC1.Models/Artists/ArtistAdd.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AlbumTrackerMVC1.Models.Artists;
+
+public class ArtistAdd 
+{
+    
+}
