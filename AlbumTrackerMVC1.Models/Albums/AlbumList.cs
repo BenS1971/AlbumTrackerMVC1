7dac614107fcd6bb13ee0bfe684fb4a31a7d5a78
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-27 17:51:56 -0400

    More model stuff.

--- AlbumTrackerMVC1.Models/Albums/AlbumList.cs
+++ AlbumTrackerMVC1.Models/Albums/AlbumList.cs
@@ -8,7 +8,10 @@ public class AlbumList
      public int AlbumId {get; set; }
      public required string AlbumName {get; set;}
 
-     
+     public string? ArtistName { get; set; }
+     public string? Format { get; set; }
+     public string? Genre { get; set; }
+     public DateTime ReleaseYear { get; set;}
 }
 
 
