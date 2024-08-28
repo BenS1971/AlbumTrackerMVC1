34ded367c1d153e7f8cd26782d495526dd086f69
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-28 18:40:56 -0400

    Removing STILL more yelllow stuff...

--- AlbumTrackerMVC1.Models/Songs/ListSongs.cs
+++ AlbumTrackerMVC1.Models/Songs/ListSongs.cs
@@ -1,16 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace AlbumTrackerMVC1.Models.Songs;
-
-// This will let the user SEE the songs that was entered.
-
-public class ListSongs
+namespace AlbumTrackerMVC1.Models.Songs
 {
-   [Display(Name = "Song Name")]
-   public required string SongName { get; set;}
-   [Display(Name = "Artist Name")]
-   public string? ArtistName { get; set;}
-   [Display(Name = "Song Duration")]
-   public DateTime SongDuration { get; set;}
-}
+    // This will let the user SEE the songs that was entered.
 
+    public class ListSongs
+    {
+        [Display(Name = "Song Name")]
+        public required string SongName { get; set; }
+        [Display(Name = "Artist Name")]
+        public string? ArtistName { get; set; }
+        [Display(Name = "Song Duration")]
+        public DateTime SongDuration { get; set; }
+    }
+
+}
