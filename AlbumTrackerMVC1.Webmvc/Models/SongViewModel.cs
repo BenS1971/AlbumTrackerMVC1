214e7d66f35fe8d7e726dd8fe9b4bc157b97eba0
Ben Strycker <bgstrycker1971@gmail.com>
2024-08-28 19:53:30 -0400

    More little itty-bitty updates.

--- AlbumTrackerMVC1.Webmvc/Models/SongViewModel.cs
+++ AlbumTrackerMVC1.Webmvc/Models/SongViewModel.cs
@@ -2,7 +2,7 @@ namespace AlbumTrackerMVC1.Webmvc.Models
 {
     public class SongViewModel
     {
-        required public string SongName { get; set; }
+        public string? SongName { get; set; }
         public string? ArtistName { get; set; }
         public string? AlbumName { get; set; }
         public DateTime SongDuration { get; set; }
